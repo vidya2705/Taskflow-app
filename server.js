730eb3fd9e54252784cd668be9fcdcd3c6af8cb7
vidya2705 <vidya2705vidya@gmail.com>
2026-04-03 22:14:30 +0530

    Finial upgrade

--- server.js
+++ server.js
@@ -6,30 +6,46 @@ app.use(express.static(__dirname));
 
 let tasks = [];
 
-// Home route
+// Home
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-// Get all tasks
+// Get tasks
 app.get("/tasks", (req, res) => {
     res.json(tasks);
 });
 
 // Add task
 app.post("/tasks", (req, res) => {
-    tasks.push(req.body);
+    tasks.push({
+        task: req.body.task,
+        priority: req.body.priority,
+        time: req.body.time,
+        done: false
+    });
     res.json({ message: "Task added" });
+});
+
+// Toggle done
+app.put("/tasks/:index", (req, res) => {
+    const index = req.params.index;
+    tasks[index].done = !tasks[index].done;
+    res.send("Updated");
 });
 
 // Delete task
 app.delete("/tasks/:index", (req, res) => {
-    const index = req.params.index;
-    tasks.splice(index, 1);
-    res.json({ message: "Task deleted" });
+    tasks.splice(req.params.index, 1);
+    res.send("Deleted");
 });
 
-// Start server
+// Clear all
+app.delete("/tasks", (req, res) => {
+    tasks = [];
+    res.send("All cleared");
+});
+
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
 });
