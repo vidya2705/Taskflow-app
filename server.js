bcbd8c6e2f27f6404909f09d5e29084e54db4504
vidya2705 <vidya2705vidya@gmail.com>
2026-04-03 21:39:26 +0530

    Added delete feature

--- server.js
+++ server.js
@@ -2,41 +2,34 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-<<<<<<< HEAD
 app.use(express.static(__dirname));
-=======
->>>>>>> 1511165733aea334968d00fde21f45ab4d5903ed
 
 let tasks = [];
 
+// Home route
 app.get("/", (req, res) => {
-<<<<<<< HEAD
     res.sendFile(__dirname + "/index.html");
-=======
-    res.send("TaskFlow App Running");
->>>>>>> 1511165733aea334968d00fde21f45ab4d5903ed
 });
 
+// Get all tasks
 app.get("/tasks", (req, res) => {
     res.json(tasks);
 });
 
+// Add task
 app.post("/tasks", (req, res) => {
     tasks.push(req.body);
-    app.delete("/tasks/:index", (req, res) => {
-    tasks.splice(req.params.index, 1);
-    res.send("Task deleted");
-});
-<<<<<<< HEAD
     res.json({ message: "Task added" });
 });
 
+// Delete task
+app.delete("/tasks/:index", (req, res) => {
+    const index = req.params.index;
+    tasks.splice(index, 1);
+    res.json({ message: "Task deleted" });
+});
+
+// Start server
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
 });
-=======
-    res.send("Task Added");
-});
-
-app.listen(3000, () => console.log("Server running on port 3000"));
->>>>>>> 1511165733aea334968d00fde21f45ab4d5903ed
